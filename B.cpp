4b9b2330a46d88c858aa60acb0399e80f0a2ed9a
Joao Veronezi <39039823+joaoveronezi@users.noreply.github.com>
2019-09-15 16:29:08 -0300

    Criação do Método MB3()

--- B.cpp
+++ B.cpp
@@ -7,9 +7,11 @@ class B {
         void MB1() {
             cout << "MB1" << endl;
         }
-
         void MB2() {
             cout << "MB2" << endl;
+        }
+        void MB3() {
+            cout << "MB3" << endl;
         }
     private:
         int B1;
