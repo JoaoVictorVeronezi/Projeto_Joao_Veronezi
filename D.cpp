fd880d0b1eeda034fdd39d13b663dbf274c39a0a
Joao Veronezi <39039823+joaoveronezi@users.noreply.github.com>
2019-09-15 16:30:48 -0300

    Criação da Classe D

---
+++ D.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class D {
+    public:
+        void MD1() {
+            cout << "MC1" << endl;
+        }
+        void MD2() {
+            cout << "MC2" << endl;
+        }
+    private:
+        string D1;
+        int D2;
+};
